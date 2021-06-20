81df797f9ca58e35f75c2f3d77af135d761ca50d
Tatiana Nekrasova <tia8cat@gmail.com>
2021-06-20 14:35:08 +0500

    updated files

--- app.py
+++ app.py
@@ -5,40 +5,61 @@ import pandas as pd
 import os
 import csv
 
-
 app = Flask(__name__, static_url_path='/static')
 
 def root_dir():  # pragma: no cover
     return os.path.abspath(os.path.dirname(__file__))
 root = root_dir() + '/'
 
-ab = ['Стрессоустойчивость',
+ab = [
+    'Стрессоустойчивость',
     'Коммуникабельность',
     'Знание ПК',
     'Знание английского языкa',
+    'Умение работать в команде',
 ]
 
-df = pd.DataFrame({'Фамилия Имя': ['Иванов Иван', 'Петров Петр', 'Иванов Сергей', 'Петрова Галина', 'Сидоров Петр'],
-                   'Ключевые навыки': ['стрессоустойчивость', 'стрессоустойчивость', 'стрессоустойчивость', 'стрессоустойчивость', 'стрессоустойчивость'],
-                   'Город': ['Москва', 'Санкт Петербург', 'Екатеринбург', 'Москва', 'Москва'],
-                   'Стаж': ['1', '5', '2', '1', '0']})
+def fmt(c):
+    if c.startswith('https'):
+        c = '<a href="%s" target=_blank>Ссылка</a>' % (c)
+    return c
 
-@app.route('/', methods=['get'])
-def index():
-    q = request.args.get('q')
 
-    f = open('resume.csv', encoding="utf-8")
+def filter_table(filename, search, ofs, total):
+    f = open(root+filename, encoding="utf-8")
     rows = csv.DictReader(f, delimiter=',', quotechar='"')
     fields = rows.fieldnames
 
     data = {}
+
     data['headers'] = fields
     data['rows'] = []
 
+    res = []
     for row in rows:
-        data['rows'].append( [str(x) for x in row.values()] )
+        if search=='' or any([search.lower() in str(x).lower() for x in row.values()]):
+            res.append([fmt(x) for x in row.values()])
 
-    return render_template('index.html', tables=[df.to_html(classes='data')], titles=df.columns.values, ab=ab, data=data, q=q)
+    data['rows'] = res[ofs:ofs+total]
+
+    return data
+
+
+@app.route('/', methods=['get'])
+def index():
+    q = request.args.get('q') or ''
+
+    k = list(map(int,request.args.getlist('k')))
+
+    ofs = 0
+    per_page = 25
+
+    vt = filter_table('vacancies.csv', q, ofs, per_page)
+    ct = filter_table('resume.csv', q, ofs, per_page)
+
+    tables = {'vt':{'data':vt}, 'ct':{'data':ct}}
+
+    return render_template('index.html', ab=ab, q=q, k=k, tables=tables)
 
 if __name__ == '__main__':
     app.run(debug=True)
